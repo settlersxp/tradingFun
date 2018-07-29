7cc29e5df807ebb1d81b15ee6a5aa066aa7a334b
Cliseru Gabriel <settlersxp@gmail.com>
2018-07-29 23:52:03 +0200

    RSI implementation v0.1

--- main.py
+++ main.py
@@ -41,11 +41,13 @@ class Helper:
 
 class Interval:
     internalData = {
-        'ohlc': {'o':0, 'h':0, 'l':0, 'c':0},
+        'ohlc': {'o': 0, 'h': 0, 'l': 0, 'c': 0},
         'durations': [],
         'isUptrend': True
     }
-    currentInterval = []
+
+    allCandles = []
+    tempSection = []
 
     periodDuration = None
     dateDuration = None
@@ -62,32 +64,39 @@ class Interval:
 
         self.dateDuration = dateDuration
 
-    def add(self, candle):
-        if len(self.currentInterval) == 0:
-            self.currentInterval.append(candle)
+    def add(self, candle, index):
+        if len(self.tempSection) == 0:
+            self.tempSection.append(candle)
             self.internalData['ohlc']['o'] = candle['o']
             self.internalData['ohlc']['c'] = candle['c']
 
             if candle['c'] > candle['o']:
-                #uptrend
+                # uptrend
                 self.internalData['ohlc']['h'] = candle['c']
                 self.internalData['ohlc']['l'] = candle['o']
             else:
-                #downtrend
+                # downtrend
                 self.internalData['isUptrend'] = False
                 self.internalData['ohlc']['l'] = candle['c']
                 self.internalData['ohlc']['h'] = candle['o']
 
             return
 
-        if mktime(candle['d']) - mktime(self.currentInterval[0]['d']) < self.periodDuration:
-            self.currentInterval.append(candle)
+        if mktime(candle['d']) - mktime(self.tempSection[0]['d']) < self.periodDuration:
+            self.tempSection.append(candle)
         else:
-            self.internalData['durations'].append(self.currentInterval.copy())
-            self.currentInterval.clear()
-            self.currentInterval.append(candle)
-            #TODO: implement dinamic period duration. Somtimes candles come with a slight delay, for example every 322s vs 300s. This can lead to big problems down the line
-            #TODO: what happens when a new candle is created? What information gets copied?
+            self.internalData['durations'].append(self.tempSection.copy())
+            self.allCandles.append(self.internalData.copy())
+
+            self.internalData = {
+                'ohlc': {'o': 0, 'h': 0, 'l': 0, 'c': 0},
+                'durations': [],
+                'isUptrend': True
+            }
+            self.tempSection.clear()
+            # TODO: implement indexes for the received candles. No need co copy the data all the time
+            # TODO: implement dinamic period duration. Somtimes candles come with a slight delay, for example every 322s vs 300s. This can lead to big problems down the line
+            # TODO: what happens when a new candle is created? What information gets copied?
 
         # update the new closing, this happens always
         self.internalData['ohlc']['c'] = candle['c']
@@ -112,16 +121,47 @@ class Interval:
         return
 
 
-# helper = Helper()
+class Calculations(Interval):
+    RSIholder = {}
+
+    def __init__(self, dateType, dateDuration):
+        super().__init__(dateType, dateDuration)
+
+    def add(self, candle, index):
+        Interval.add(self, candle, index)
+        self.RSI(10)
+
+    # calculate the RSI of the desired duration and metric. By default c (close) is used.
+    # Available options: o,h,l
+    # for open, high, low
+    def RSI(self, duration, metric='c'):
+        # not enough candles
+        if len(self.allCandles) < duration + 1:
+            return
+
+        if len(self.allCandles) == duration:
+            self.RSIholder[duration + '-' + metric] = {
+                'initial': 0,
+                'relevant': 0,
+                'old': []
+            }
+
+        print('qwe')
+        pass
+
+
+helper = Helper()
 # helper.process_files(['USDCHF-2018-01.csv', 'USDCHF-2018-02.csv', 'USDCHF-2018-04.csv', 'USDCHF-2018-05.csv', 'USDCHF-2018-06.csv'])
-# helper.process_files(['USDCHF-2018-01.csv'])
-data = np.load(processedFolder + 'min-USDCHF-2018-01.csv.npy')
+fileName = 'min-USDCHF-2018-02.csv'
+# helper.process_files([fileName])
+data = np.load(processedFolder + fileName + '.npy')
 
-fiveMin = Interval('minute', 5)
-tenMin = Interval('minute', 10)
-fifteenMin = Interval('minute', 15)
-oneHour = Interval('hour', 1)
+fiveMin = Calculations('minute', 5)
 
+i = 0
 # simulate the candle ticking
 for candle in data:
-    fiveMin.add(candle)
+    fiveMin.add(candle, i)
+    i += 1
+
+print('asd')
